9afdeb0293b20a76f13b77f7cbd8c863b11d18fe
Andrei R da Silva <silva.andrei@outlook.com.br>
2024-07-08 16:19:03 -0300

    iniciando projeto fueltrack

---
+++ src/main/java/dev/andrei/fueltrack/FueltrackApplication.java
@@ -0,0 +1,13 @@
+package dev.andrei.fueltrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FueltrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FueltrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andrei/fueltrack/FulltrackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrei.fueltrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulltrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
